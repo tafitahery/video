3ac522155780c01bc76f794e3289f5bafa501fbd
Tafita Hery <tafitahery@gmail.com>
2023-07-24 06:39:39 +0300

    React Navbar Design

--- video-react-ui/src/components/Navbar.jsx
+++ video-react-ui/src/components/Navbar.jsx
@@ -1,3 +1,69 @@
+import { styled } from 'styled-components';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  height: 56px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: ${({ theme }) => theme.bgLighter};
+  height: 100%;
+  padding: 0 20px;
+  position: relative;
+`;
+
+const Search = styled.div`
+  width: 40%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+  background-color: transparent;
+`;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Search>
+          <Input placeholder="Search" />
+          <SearchOutlinedIcon />
+        </Search>
+        <Button>
+          <AccountCircleOutlinedIcon />
+          SIGN IN
+        </Button>
+      </Wrapper>
+    </Container>
+  );
 }
