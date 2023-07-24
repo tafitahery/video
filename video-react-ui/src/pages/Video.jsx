3d77331aeee5280a6c721ee3dc5aa2ef3342d8e3
Tafita Hery <tafitahery@gmail.com>
2023-07-24 18:18:49 +0300

    React Video Page Design

--- video-react-ui/src/pages/Video.jsx
+++ video-react-ui/src/pages/Video.jsx
@@ -1,3 +1,167 @@
+import { styled } from 'styled-components';
+import {
+  AddTaskOutlined,
+  ReplyOutlined,
+  ThumbDownAltOutlined,
+  ThumbUpOutlined,
+} from '@mui/icons-material';
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+
+const VideoWrapper = styled.div``;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Image = styled.img`
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+`;
+
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.span`
+  font-weight: 500;
+`;
+
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 12px;
+`;
+
+const Desc = styled.p`
+  font-size: 14px;
+`;
+
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+
+const Recommandation = styled.div`
+  flex: 2;
+`;
+
 export default function Video() {
-  return <div>Video</div>;
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="420"
+            src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+            title="YouTube video player"
+            // eslint-disable-next-line react/no-unknown-property
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>7,948,154 views . Jun, 22 2022</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlined />
+              123
+            </Button>
+            <Button>
+              <ThumbDownAltOutlined />
+              Dislike
+            </Button>
+            <Button>
+              <ReplyOutlined />
+              Share
+            </Button>
+            <Button>
+              <AddTaskOutlined />
+              Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+            <ChannelDetail>
+              <ChannelName>Lebo Dev</ChannelName>
+              <ChannelCounter>200K Subcribers</ChannelCounter>
+              <Desc>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perferendis, ad corrupti. Esse a non nam incidunt nesciunt,
+                quibusdam tenetur atque ipsum, in repudiandae reprehenderit
+                ullam temporibus dicta! Rem, omnis veritatis?
+              </Desc>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>Subscribe</Subscribe>
+        </Channel>
+      </Content>
+      <Recommandation>Recommandation</Recommandation>
+    </Container>
+  );
 }
