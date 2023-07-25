20622b0d87513aace3867fb11fba323036171104
Tafita Hery <tafitahery@gmail.com>
2023-07-25 17:48:40 +0300

    React Login Page Design

--- video-react-ui/src/components/Menu.jsx
+++ video-react-ui/src/components/Menu.jsx
@@ -117,10 +117,12 @@ export default function Menu({ setDarkMode, darkMode }) {
         <Hr />
         <Login>
           Sign in to like videos, comment, and subscribe.
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
+          <Link to="signin" style={{ textDecoration: 'none' }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>
         </Login>
         <Hr />
         <Title>BEST OF LEBOTUBE</Title>
--- video-react-ui/src/components/Navbar.jsx
+++ video-react-ui/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   position: sticky;
@@ -59,10 +60,12 @@ export default function Navbar() {
           <Input placeholder="Search" />
           <SearchOutlinedIcon />
         </Search>
-        <Button>
-          <AccountCircleOutlinedIcon />
-          SIGN IN
-        </Button>
+        <Link to="signin" style={{ textDecoration: 'none' }}>
+          <Button>
+            <AccountCircleOutlinedIcon />
+            SIGN IN
+          </Button>
+        </Link>
       </Wrapper>
     </Container>
   );
