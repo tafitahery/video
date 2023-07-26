980ac3808a849b2b4589377bf83c8711d3525bd4
Tafita Hery <tafitahery@gmail.com>
2023-07-26 14:25:21 +0300

    How to Create a Rest API using Node.js and MongoDB

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    subcribers: {
+      type: Number,
+      default: 0,
+    },
+    subcribedUsers: {
+      type: [String],
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', UserSchema);
