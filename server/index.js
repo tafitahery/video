fe0a9a247dd704b3343a67b458aeb5f6e2e9d5cd
Tafita Hery <tafitahery@gmail.com>
2023-07-26 13:37:21 +0300

    Node.js How to Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log('Connect to DB');
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 app.listen(8800, () => {
-  console.log('Connected');
+  connect();
+  console.log('Connected to the server');
 });
