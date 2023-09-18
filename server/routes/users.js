703c8e8f32d35086f456d6eeda173d8bf611c88b
Tafita Hery <tafitahery@gmail.com>
2023-09-18 18:43:51 +0300

    Node.js MongoDB JWT Cookie Authentification

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,35 @@
 import express from 'express';
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  update,
+} from '../controllers/user.js';
 
 const router = express.Router();
+
+// update user
+router.put('/:id', update);
+
+// delete user
+router.delete('/:id', deleteUser);
+
+// get a user
+router.get('/find/:id', getUser);
+
+// subscribe a user
+router.put('/sub/:id', subscribe);
+
+// unsubscribe a user
+router.put('/unsub/:id', unsubscribe);
+
+// like a video
+router.put('/like/:videoId', like);
+
+// dislike a video
+router.put('/dislike/:videoId', dislike);
 
 export default router;
